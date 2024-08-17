a53156a93b6ffe08f48ebbb8eac86886d706e468
lnix1 <louis.nix2@gmail.com>
2024-08-17 00:52:04 -0700

    Added labels for sub-page icons, fixed resume download to work and added CV to images

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,52 @@
 import * as React from 'react'
 import { StaticImage } from "gatsby-plugin-image"
+import FileSaver from 'file-saver';
+import MyPDF from '../images/Resume_Nix_L.pdf'; 
 
 export function Navbar() {
   return (
-    <a href="/">
-      <div class="invert">
-        <StaticImage
-          src="../images/home.png"
-          alt="Navbar Image"
-          placeholder="blurred"
-          layout="fixed"
-          height={60}
-          width={60}
-        />
-      </div>
-    </a>
+    <nav class="flex items-center justify-between mb-40">
+        <a href="/">
+          <div class="invert">
+            <StaticImage
+              src="../images/home.png"
+              alt="Navbar Image"
+              placeholder="blurred"
+              layout="fixed"
+              height={40}
+              width={40}
+            />
+          </div>
+        </a>
+        <a href="https://github.com/lnix1">
+          <StaticImage
+            src="../images/github-mark-white.png"
+            alt=""
+            layout="fixed"
+            height={40}
+            width={40}
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/louisnixii/">
+          <StaticImage
+            src="../images/In-White-72.png"
+            alt=""
+            layout="fixed"
+            height={40}
+            width={40}
+          />
+        </a>
+        <a class="invert" href={MyPDF} download="Resume_Nix_L.pdf">
+          <StaticImage
+            //Credit to spaceman.design, via flaticon.com
+            src="../images/resume.png"
+            alt=""
+            layout="fixed"
+            height={40}
+            width={40}
+          />
+        </a>
+    </nav>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Headshot from '../components/headshot'
 import Navbar from '../components/navbar'
 import BackgroundSection from '../components/bg'
-import BlurredBg from '../components/blurred_bg'
 import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
@@ -15,28 +14,7 @@ const IndexPage = () => {
 
             <div class="container min-h-screen mx-auto px-4">
 
-                <nav class="flex items-center justify-between mb-40">
-                    <Navbar />
-                    <a href="https://github.com/lnix1">
-                      <StaticImage
-                        src="../images/github-mark-white.png"
-                        alt=""
-                        layout="fixed"
-                        height={60}
-                        width={60}
-                      />
-                    </a>
-                    <a href="https://www.linkedin.com/in/louisnixii/">
-                      <StaticImage
-                        src="../images/In-White-72.png"
-                        alt=""
-                        layout="fixed"
-                        height={60}
-                        width={60}
-                      />
-                    </a>
-                    <button class="px-7 py-3 md:px-9 md:py-4 bg-gray-500 font-medium md:font-semibold text-gray-50 text-md rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500">CV</button>
-                </nav>
+                <Navbar />
 
                 <div class="text-center">
                     <div class="flex justify-center mb-16">
@@ -45,28 +23,29 @@ const IndexPage = () => {
 
                     <div class="rounded-lg">
 
-                      <h1 class="font-normal text-gray-300 text-lg md:text-5xl uppercase mb-8 opacity-100">Louis Nix II</h1>
+                      <h1 class="text-gray-200 text-lg md:text-6xl mb-8 opacity-100">Louis Nix II</h1>
 
-                      <h1 class="font-normal text-gray-300 text-4xl md:text-6xl leading-none mb-8">Full Stack Developer & Data Analyst</h1>
+                      <h1 class="text-gray-200 text-4xl md:text-4xl leading-none mb-8">Full Stack Developer & Data Analyst</h1>
 
                     </div>
 
                 </div>
 
-                <div class="text-center">
+                <div class="text-center py-10">
 
-                    <div class="grid grid-cols-3 gap-4 justify-items-center mb-16 p-10">
-
-                      <a href="/" class="invert">
-                        <StaticImage
-                          //Credits to spaceman.design, via flaticon.com
-                          src="../images/resume.png"
-                          alt=""
-                          layout="fixed"
-                          height={60}
-                          width={60}
-                        />
-                      </a>
+                    <div class="grid grid-cols-3 gap-4 justify-items-center">
+                      <div class="flex items-center">
+                        <a href="/" class="invert">
+                          <StaticImage
+                            //Credits to Freepik, via flaticon.com
+                            src="../images/resume_2.png"
+                            alt=""
+                            layout="fixed"
+                            height={60}
+                            width={60}
+                          />
+                        </a>
+                      </div>
                       <a href="/" class="invert">
                         <StaticImage
                           //Credits to Freepik, via flaticon.com
@@ -89,14 +68,25 @@ const IndexPage = () => {
                       </a>
                     </div>
 
+                    <div class="grid grid-cols-3 gap-4 justify-items-center">
+                      <div>
+                        <h1 class="text-gray-200 md:text-2xl">Experience</h1>
+                      </div>
+                      <div>
+                        <h1 class="text-gray-200 md:text-2xl">Projects</h1>
+                      </div>
+                      <div>
+                        <h1 class="text-gray-200 md:text-2xl">Blog</h1>
+                      </div>
+                    </div>
                 </div>
 
-                <div class="text-center">
+                <div class="text-center py-10">
 
 
                     <div class="rounded-lg">
 
-                      <a href="/" class="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold bg-gray-500 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500">Contact me</a>
+                      <a href="/" class="px-7 py-3 md:px-9 md:py-4 md:font-semibold bg-gray-500 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500">Contact me</a>
 
                     </div>
 
